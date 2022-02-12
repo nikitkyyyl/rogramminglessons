dbe861828d470296202f354d9f6d079201b018e3
nikitkyyyl <nikitos_hocke@mail.ru>
2022-02-12 12:00:05 +0300

    задание 2

---
+++ zadanie2.js
@@ -0,0 +1,18 @@
+let age;
+console.log (typeof age); 
+let TF = false;
+console.log (typeof TF); 
+let sim = Symbol();
+console.log (typeof sim); 
+let user = {age:30};
+console.log (typeof user); 
+let us = function(){};
+console.log (typeof us); 
+let cif = 1937n;
+console.log (typeof cif); 
+let str = 'меня завут никита';
+console.log (typeof str);
+let num = 1937;
+console.log (typeof num);
+
+
